686cc787e18193e3268ae0cc97815251b0302e50
piotrgarncarz <piotrgarncarz2006@gmail.com>
2021-10-17 19:08:23 +0200

    finished render function

--- Engine/engine.py
+++ Engine/engine.py
@@ -1,30 +1,49 @@
 #!/usr/bin/python3
 import pygame as pg
-from pygame.locals import *
 import os
 import sys
+import numpy as np
 
+#colors that can be used anywhere in this project
 colors= {
         'black': (0, 0, 0),
         'white': (255, 255, 255),
         }
 
 class Engine:
-    def __init__(self, w, h, bg_layer, bg_layer2, world):
+    def __init__(self, w, h, title, bg_img, world):
         self.w= w
         self.h= h
-        self.bg= bg_layer,
-        self.bg2= bg_layer2
+        self.bg_img= pg.image.load(bg_img)
+        self.loadImages(world)
         self.world= world
+
+        #initializes pygame
         pg.init()
+        #creates pygame window
         self.win= pg.display.set_mode((self.w, self.h))
-        pg.display.set_caption('')
+        pg.display.set_caption(title)
+    #moves viewport by moving it's content
     def mv_viewport(self, x, y):
-        for e in self.bg:
-            for e2 in e:
-                e.x, e.y= x+ e.x, y+ e.y
         for e in self.world:
             e.x, e.y= x+e.x, y+ e.y
+    def drawBgImage(self):
+        for x in np.arange(0, self.w+ self.bg_img.get_width(), self.bg_img.get_width()):
+            for y in np.arange(0, self.h+ self.bg_img.get_height(), self.bg_img.get_height()):
+                self.win.blit(self.bg_img, (x, y))
+    def loadImages(self, elements):
+        for e in elements:
+            print(e)
+            e['img']= pg.image.load(e['img'])
+    #draws content from list
+    def draw(self, elements):
+        for e in elements:
+            self.win.blit(e['img'], e['pos'])
+    #renders content
     def render(self):
-        #TODO: add draw bg and terrain functionality
+        self.drawBgImage()
+        #draws world
+        self.draw(self.world)
+
+        #updates window content
         pg.display.update()
--- Engine/game.py
+++ Engine/game.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 from engine import *
 
-engine= Engine(500, 500, [ ['grass', 500, 500, ['img', os.path.join(os.path.expanduser('~'), 'Py', 'konkurs', 'Assets', 'Images', 'grass.png')]] ], [], [])
+#returns path of this directory
+dirname= os.path.dirname(__file__)
+
+#contains world content
+world= [
+        # {
+        #     'pos': (20, 20),
+        #     'img': os.path.join(dirname, '..', 'Assets', 'Images', 'sample.png')
+        #     }
+        ]
+
+engine= Engine(w= 1000, h= 800, title= '', bg_img= os.path.join(dirname, '..', 'Assets', 'Images', 'grass.png'), world= world)
+
+#creates pygame clock object
 clock= pg.time.Clock()
+
+#game loop
 while True:
+    #handles close window event
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
+    #renders content
     engine.render()
+    #enforcing fps
     clock.tick(60)
